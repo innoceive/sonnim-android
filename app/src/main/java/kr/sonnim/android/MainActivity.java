5d28ba86fee5616cf99d4297d5d064327140934c
GoeGosu <geogosu@sonnim.kr>
2016-09-29 12:26:33 +0900

    #fabric 설정 완료 #beta 올림 #이제 개발 시작

--- app/src/main/java/kr/sonnim/android/MainActivity.java
+++ app/src/main/java/kr/sonnim/android/MainActivity.java
@@ -2,12 +2,16 @@ package kr.sonnim.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+        System.out.println("태후니 보고인니");
     }
 }
